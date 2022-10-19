af971e74b523fe7a0ee0e2930e177d34bfb38657
carami <carami@gmail.com>
2022-10-19 21:29:46 +0900

    update restapi

--- todomvc/src/main/java/com/exam/todomvc/config/MvcConfig.java
+++ todomvc/src/main/java/com/exam/todomvc/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package com.exam.todomvc.config;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -17,27 +19,25 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
-
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.exam.todomvc"})
-public class MvcConfig implements WebMvcConfigurer{
-	
+@ComponentScan(basePackages = { "com.exam.todomvc" })
+public class MvcConfig implements WebMvcConfigurer {
+
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
 	}
+
 	@Override
 	public void configureViewResolvers(ViewResolverRegistry registry) {
 		registry.jsp("/WEB-INF/views/", ".jsp");
 	}
-	
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-				.allowedOriginPatterns("*")
-				.allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
-				.allowCredentials(true);
+		registry.addMapping("/**").allowedOriginPatterns("*")
+				.allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowCredentials(true);
 	}
 
 	@Override
@@ -46,8 +46,7 @@ public class MvcConfig implements WebMvcConfigurer{
 		final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 		final ObjectMapper objectMapper = new ObjectMapper();
 
-		objectMapper.configure(com.fasterxml.jackson.databind.SerializationFeature.
-				WRITE_DATES_AS_TIMESTAMPS, false);
+		objectMapper.configure(com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 		objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
 		objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
 		converter.setObjectMapper(objectMapper);
@@ -55,7 +54,12 @@ public class MvcConfig implements WebMvcConfigurer{
 		builder.serializationInclusion(JsonInclude.Include.NON_NULL);
 		builder.serializationInclusion(JsonInclude.Include.NON_EMPTY);
 
-		// Add settings to converter, builder
+//		List<MediaType> mediatype = new ArrayList<MediaType>();
+//		mediatype.add(MediaType.APPLICATION_JSON);
+//
+//		// Add settings to converter, builder
+//		converter.setSupportedMediaTypes(mediatype);
+		
 		converters.add(converter);
 		converters.add(new MappingJackson2HttpMessageConverter(builder.build()));
 	}
