6ec9af538f61ed48015bc4f5132c7d7dec870c4c
carami <carami@nate.com>
2022-11-04 17:13:45 +0900

    final update

--- todomvc/src/main/java/com/exam/todomvc/config/SwaggerConfig.java
+++ todomvc/src/main/java/com/exam/todomvc/config/SwaggerConfig.java
@@ -1,17 +1,23 @@
 package com.exam.todomvc.config;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
-import io.swagger.models.Contact;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -44,6 +50,8 @@ public class SwaggerConfig {
 	// swagger 설정.
 	public Docket getDocket(String groupName, Predicate<String> predicate) {
 		return new Docket(DocumentationType.SWAGGER_2)
+				.securityContexts(Arrays.asList(securityContext())) // bearer인증 설정
+				.securitySchemes(Arrays.asList(apiKey())) // bearer 인증 설정 
 				.apiInfo(apiInfo())
 				.groupName(groupName)
 				.select()
@@ -53,6 +61,24 @@ public class SwaggerConfig {
 				.build();
 	}
 	
-	
+// bearer인증 설정
+    
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+            .securityReferences(defaultAuth())
+            .build();
+    }
+    
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+    
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+    
 
 }
