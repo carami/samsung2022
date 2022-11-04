89f4da69c7768a055d9725cb3275b7e6895a2921
carami <carami@nate.com>
2022-11-04 16:16:32 +0900

    시큐리티 설정 추가

--- todojpa/src/main/java/com/exam/todojpa/controller/HomeController.java
+++ todojpa/src/main/java/com/exam/todojpa/controller/HomeController.java
@@ -1,15 +1,17 @@
-package com.exam.todojpa;
+package com.exam.todojpa.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.security.Principal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.exam.todojpa.domain.User;
+import com.exam.todojpa.service.UserService;
 
 /**
  * Handles requests for the application home page.
@@ -19,20 +21,18 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home( Principal principal) {
+		logger.info("Principal : {}", principal);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+		if(principal != null) {
+			User user = userService.getUser(principal.getName());
+			
+			logger.info("user : {} ",user);
+		}
 		return "home";
 	}
 	
--- todojpa/src/main/java/com/exam/todojpa/service/CustomUserDetailsService.java
+++ todojpa/src/main/java/com/exam/todojpa/service/CustomUserDetailsService.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.exam.todojpa.domain.User;
 import com.exam.todojpa.repository.UserRepository;
-
+@Service
+@Transactional
 public class CustomUserDetailsService implements UserDetailsService {
 	
 	@Autowired
