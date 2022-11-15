608570fc753734aa64c97357638dbac85d8c9027
carami <carami@nate.com>
2022-11-15 17:22:49 +0900

    todomvc 추가

---
+++ todomvc/src/main/java/com/exam/todomvc/dao/TodoDao.java
@@ -0,0 +1,8 @@
+package com.exam.todomvc.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TodoDao {
+
+}
---
+++ todomvc/src/main/resources/db/create-db.sql
@@ -0,0 +1,6 @@
+create TABLE todos (
+    id INTEGER auto_increment PRIMARY KEY,
+    todo VARCHAR(255),
+    done boolean
+);
+
