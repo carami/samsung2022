80c9c61f71edcd8c8fc380261a3bec5d6ac17fe2
carami <carami@nate.com>
2022-11-17 16:15:55 +0900

    spring jpa를 이용한 등록 수정 삭제 추가

--- todojpa/src/main/java/com/exam/todojpa/TodoJapRun.java
+++ todojpa/src/main/java/com/exam/todojpa/TodoJapRun.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.exam.todojpa.config.ApplicationConfig;
 import com.exam.todojpa.domain.Todo;
 import com.exam.todojpa.repository.TodoRepository;
+import com.exam.todojpa.service.TodoService;
 
 public class TodoJapRun {
 
@@ -14,10 +15,33 @@ public class TodoJapRun {
 		TodoRepository todoRepository = ctx.getBean(TodoRepository.class);
 		
 		//1. 1건 저장하기. 
+//		
+//		Todo todo = new Todo("jpa 재밌쬬??!!");
+//		
+//		todo = todoRepository.save(todo);
+//		
+//		//2. id에 해당하는 todo 조회 
+//		Todo findTodo = todoRepository.findById(10L).orElseThrow();
+//		
+//		System.out.println(findTodo);
+//		
+//		Todo findTodo2 = todoRepository.getById(10L);
+//		System.out.println(findTodo);
 		
-		Todo todo = new Todo("jpa 재밌쬬??!!");
+//		//3. update 
+		TodoService service = ctx.getBean(TodoService.class);
+//		
+//		System.out.println("todoService start");
+//		Todo resultTodo1 = service.updateTodo(10L);
+//		System.out.println(resultTodo1);
+//		System.out.println("todoService end");
+//		
 		
-		todoRepository.save(todo);
+		//4. 삭제
+//		Todo findTodo2 = todoRepository.getById(10L);
+//		todoRepository.delete(findTodo2);
+		
+		service.removeTodo(14L);
 		
 	}
 
--- todojpa/src/main/java/com/exam/todojpa/service/TodoService.java
+++ todojpa/src/main/java/com/exam/todojpa/service/TodoService.java
@@ -1,5 +1,41 @@
 package com.exam.todojpa.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.exam.todojpa.domain.Todo;
+import com.exam.todojpa.repository.TodoRepository;
+
+@Service
 public class TodoService {
+	
+	@Autowired
+	private TodoRepository todoRepository;
+	
+	@Transactional
+	public Todo updateTodo(Long id) {
+		System.out.println("service  updateTodo start!! ");
+		Todo todo = null;
+		try {
+			todo = todoRepository.findById(id).orElseThrow();
+			
+			todo.setDone(!todo.isDone());
+		
+		}finally {
+			System.out.println("service  updateTodo end!!!! ");
+		}
+		return todo;
+	}
+	
+	@Transactional
+	public void removeTodo(Long id) {
+		Optional<Todo> result =	todoRepository.findById(id);
+//		if(result.isEmpty())
+//			return false;
+		todoRepository.delete(result.get());		
+	}
 
 }
