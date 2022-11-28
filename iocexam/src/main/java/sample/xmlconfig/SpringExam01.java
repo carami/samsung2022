9e4b1c35fa9022136f2a30959201c3a3898bbab0
carami <carami@nate.com>
2022-11-28 15:22:24 +0900

    firstspring, iocexam 추가

---
+++ iocexam/src/main/java/sample/xmlconfig/SpringExam01.java
@@ -0,0 +1,15 @@
+package sample.xmlconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringExam01 {
+
+	public static void main(String[] args) {
+		System.out.println("ApplicationContext 생성전!!");
+		ApplicationContext context = new ClassPathXmlApplicationContext("exam01.xml");
+		System.out.println("ApplicationContext 생성후!!");
+		
+	}
+
+}
